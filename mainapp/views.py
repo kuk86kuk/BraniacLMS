24f4e3a33da5f5171134b74c4d45549d4ea125e3
EgorTreglazov <egort.123@yandex.ru>
2023-01-19 18:43:56 +0500

    lesson_4 Migrations

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -16,6 +18,15 @@ class NewsPageView(TemplateView):
         context[
             "news_preview"
         ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
         return context
 
 
